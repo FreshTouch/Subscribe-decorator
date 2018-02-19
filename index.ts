7acbc82804a570895aa5f93c417a0e4f44f0bdaa
Suren Navasardyan <hipparchus@ya.ru>
2018-02-19 05:46:07 +0400

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,6 @@
+import {Subscribable} from "./subscribe";
+import Subscribables = Subscribable.Subscribe;
+import Subscribe = Subscribable.Subscribe;
+import State = Subscribable.State;
+
+export {Subscribe, Subscribables, State};
